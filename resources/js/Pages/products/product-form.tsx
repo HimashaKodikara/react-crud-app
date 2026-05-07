c0a96b1ceb7d1986e5dc7083c0f4a01d0a3798ef
19840769 <kodikarah.21@uom.lk>
2026-05-07 12:05:27 +0530

    feat: add product creation form component using Inertia.js and Headless UI

--- resources/js/Pages/products/product-form.tsx
+++ resources/js/Pages/products/product-form.tsx
@@ -1,10 +1,27 @@
 import FluxLayout from "@/Layouts/FluxLayout";
-import { Head } from "@inertiajs/react";
+import { Head, Link, useForm } from "@inertiajs/react";
 import { Card, CardContent, CardHeader, CardTitle } from "@/Components/ui/card";
-import { Field, Input, Label, Textarea } from "@headlessui/react";
+import { Button, Field, Input, Label } from "@headlessui/react";
 import { CustomTextarea } from "@/Components/ui/custom-textarea";
+import { FormEventHandler } from "react";
 
 export default function CreateProduct() {
+    const { data, setData, post, processing, errors, reset } = useForm({
+        name: "",
+        description: "",
+        price: "",
+        featured_image: null as File | null,
+    });
+
+    const submit: FormEventHandler = (e) => {
+        e.preventDefault();
+
+        post(route("manageproduct.store"), {
+            forceFormData: true,
+            onSuccess: () => reset(),
+        });
+    };
+
     return (
         <FluxLayout
             header={
@@ -16,45 +33,129 @@ export default function CreateProduct() {
             <Head title="Create Product" />
 
             <div className="flex h-full flex-1 flex-col gap-4 rounded-xl p-4 ">
+                <div className="ml-auto">
+                    <Link
+                        as="button"
+                        href={route("manageproduct.index")}
+                        className="mt-4 w-fit cursor-pointer inline-flex justify-center rounded-md border border-transparent bg-blue-600 py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-gray-500 focus:ring-offset-2 disabled:opacity-50"
+                    >
+                        Back to Products
+                    </Link>
+                </div>
+
                 <Card className="w-full">
                     <CardHeader>
                         <CardTitle>Create Product</CardTitle>
                     </CardHeader>
                     <CardContent>
                         <form
+                            onSubmit={submit}
                             className="flex flex-col gap-4"
                             autoCapitalize="off"
                         >
                             <div className="grid gap-6">
                                 <Field className="grid gap-2">
-                                    {/*Product Name*/}
                                     <Label className="text-sm font-medium text-zinc-700 dark:text-zinc-300">
-                                        {" "}
                                         Product name
                                     </Label>
                                     <Input
                                         id="name"
                                         name="name"
                                         type="text"
+                                        value={data.name}
+                                        onChange={(e) =>
+                                            setData("name", e.target.value)
+                                        }
                                         placeholder="Product Name"
                                         autoFocus
                                         className="block w-full rounded-md border-0 py-1.5 text-zinc-900 shadow-sm ring-1 ring-inset ring-zinc-300 placeholder:text-zinc-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 dark:bg-zinc-800 dark:text-white dark:ring-zinc-700"
                                     ></Input>
+                                    {errors.name && (
+                                        <div className="text-sm text-red-600">
+                                            {errors.name}
+                                        </div>
+                                    )}
                                 </Field>
                                 <Field className="grid gap-2">
-                                    {/*Product Description*/}
-                                    <Label className="text-sm font-medium text-zinc-700 dark:text-zinc-300">
+                                    <Label
+                                        htmlFor="description"
+                                        className="text-sm font-medium text-zinc-700 dark:text-zinc-300"
+                                    >
                                         Product Description
                                     </Label>
-                                    <CustomTextarea />
+                                    <CustomTextarea
+                                        id="description"
+                                        rows={3}
+                                        name="description"
+                                        value={data.description}
+                                        onChange={(e) =>
+                                            setData(
+                                                "description",
+                                                e.target.value,
+                                            )
+                                        }
+                                        className="block w-full rounded-md border-0 py-1.5 text-zinc-900 shadow-sm ring-1 ring-inset ring-zinc-300 placeholder:text-zinc-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 dark:bg-zinc-800 dark:text-white dark:ring-zinc-700"
+                                    />
+                                    {errors.description && (
+                                        <div className="text-sm text-red-600">
+                                            {errors.description}
+                                        </div>
+                                    )}
+                                </Field>
+                                <Field className="grid gap-2">
+                                    <Label className="text-sm font-medium text-zinc-700 dark:text-zinc-300">
+                                        Product Price
+                                    </Label>
                                     <Input
-                                        id="name"
-                                        name="name"
+                                        id="price"
+                                        name="price"
                                         type="text"
-                                        placeholder="Product Name"
+                                        value={data.price}
+                                        onChange={(e) =>
+                                            setData("price", e.target.value)
+                                        }
+                                        placeholder="Product Price"
                                         className="block w-full rounded-md border-0 py-1.5 text-zinc-900 shadow-sm ring-1 ring-inset ring-zinc-300 placeholder:text-zinc-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 dark:bg-zinc-800 dark:text-white dark:ring-zinc-700"
                                     ></Input>
+                                    {errors.price && (
+                                        <div className="text-sm text-red-600">
+                                            {errors.price}
+                                        </div>
+                                    )}
                                 </Field>
+                                <Field className="grid gap-2">
+                                    <Label
+                                        htmlFor="featured_image"
+                                        className="text-sm font-medium text-zinc-700 dark:text-zinc-300"
+                                    >
+                                        Featured Image
+                                    </Label>
+                                    <Input
+                                        id="featured_image"
+                                        name="featured_image"
+                                        type="file"
+                                        onChange={(e) =>
+                                            setData(
+                                                "featured_image",
+                                                e.target.files
+                                                    ? e.target.files[0]
+                                                    : null,
+                                            )
+                                        }
+                                        className="block w-full rounded-md border-0 py-1.5 text-zinc-900 shadow-sm ring-1 ring-inset ring-zinc-300 placeholder:text-zinc-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 dark:bg-zinc-800 dark:text-white dark:ring-zinc-700"
+                                    ></Input>
+                                    {errors.featured_image && (
+                                        <div className="text-sm text-red-600">
+                                            {errors.featured_image}
+                                        </div>
+                                    )}
+                                </Field>
+                                <Button
+                                    type="submit"
+                                    className="mt-4 w-fit cursor-pointer inline-flex justify-center rounded-md border border-transparent bg-gray-600 py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-gray-500 focus:ring-offset-2 disabled:opacity-50"
+                                >
+                                    Save Product
+                                </Button>
                             </div>
                         </form>
                     </CardContent>
